8fb8a5ba2a175e5a80aabf540c7ec1dc2fdcf8d5
hhhfccz <hjk1938927583@163.com>
2020-08-28 19:17:32 +0800

    Update rotation_according_to_word_direction.py
    need to optimize, TODO: CTPN

--- rotation_according_to_word_direction.py
+++ rotation_according_to_word_direction.py
@@ -1,75 +1,74 @@
+# ——*——coding:utf-8——*——
+# author: 胡润龙、hhhfccz(胡珈魁) time:2020/8/25
 import numpy as np
 import cv2
+import math
+
 
 def rotated_img_with_fft(gray_img):
-    #cut & extend image so it will be easier for FFT algorithm to calculate
+    # 获取图片大小
     h, w = gray_img.shape[:2]
-    new_h = cv2.getOptimalDFTSize(h)
-    new_w = cv2.getOptimalDFTSize(w)
-    right = new_w - w
-    bottom = new_h - h
-    nimg = cv2.copyMakeBorder(gray_img, 0, bottom, 0, right, borderType=cv2.BORDER_CONSTANT, value=0)
 
-    #perform FFT
-    f = np.fft.fft2(nimg)
-    fshift = np.fft.fftshift(f)
+    # 对图像进行傅里叶变换
+    dft = np.fft.fft2(img_gray)
+    shift = np.fft.fftshift(dft)
 
-    fft_img = np.log(np.abs(fshift))
+    fft_img = np.log(np.abs(shift))
     fft_img = (fft_img - np.amin(fft_img)) / (np.amax(fft_img) - np.amin(fft_img))
-
     fft_img *= 255
 
-    #binarization
-    ret, thresh = cv2.threshold(fft_img, 150, 255, cv2.THRESH_BINARY)
+    # 边缘检测，关键问题1.需要对threshold1值做特定变换
+    thresh = cv2.Canny(fft_img.astype(np.uint8), 70, 150)
 
-    #perform Hough transform
-    thresh = thresh.astype(np.uint8)
-    lines = cv2.HoughLinesP(thresh, 1, np.pi / 180, 30, minLineLength=40, maxLineGap=100)
-    try:
+    # 霍夫变换，关键问题2，需要对函数数值做特定变换
+    lines = cv2.HoughLinesP(thresh.astype(np.uint8), 1.0, np.pi / 180, 20, minLineLength=20, maxLineGap=10)
+    if lines is not None:
         lines1 = lines[:, 0, :]
-    except Exception as e:
+    else:
         lines1 = []
-
-    # create a new image using the lines before
-    # lineimg = np.ones(nimg.shape,dtype=np.uint8)
-    # lineimg = lineimg * 255
 
     piThresh = np.pi / 180
     pi2 = np.pi / 2
     angle = 0
     for line in lines1:
-        # x1, y1, x2, y2 = line[0]
         x1, y1, x2, y2 = line
-        # cv2.line(lineimg, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        if x2 - x1 == 0:
+        if abs(x2 - x1) < 1e-2:
             continue
-
-        #little bias is allowed
         else:
-            #theta is approximately equal to tan(theta)
             theta = (y2 - y1) / (x2 - x1)
-        if abs(theta) < piThresh or abs(theta - pi2) < piThresh:
-            continue
-        else:
-            angle = abs(theta)
-            break
-    
-    angle = math.atan(angle)
-    angle = angle * (180 / np.pi)
-    print(angle)
-    # cv2.imshow("line image", lineimg)
-    center = (w // 2, h // 2)
-    height_1 = int(w * fabs(sin(radians(angle))) + h * fabs(cos(radians(angle))))
-    width_1 = int(h * fabs(sin(radians(angle))) + w * fabs(cos(radians(angle))))
+            if abs(theta) < piThresh or abs(theta - pi2) < piThresh:
+                continue
+            else:
+                angle = abs(theta)
+                break
 
-    #get the matrix to transform the image 
+    angle = math.atan(angle)
+    angle = angle / piThresh
+    center = (w // 2, h // 2)
+    height_1 = int(w * math.fabs(math.sin(math.radians(angle))) + h * math.fabs(math.cos(math.radians(angle))))
+    width_1 = int(h * math.fabs(math.sin(math.radians(angle))) + w * math.fabs(math.cos(math.radians(angle))))
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     M[0, 2] += (width_1 - w) / 2
     M[1, 2] += (height_1 - h) / 2
 
-    #rotate the image using the matrix before
-    rotated = cv2.warpAffine(gray, M, (width_1, height_1), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    cv2.imshow('rotated', rotated)
-    cv2.waitKey(0)
+    # 旋转
+    rotated = cv2.warpAffine(gray_img, M, (width_1, height_1), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    rotated = cv2.resize(rotated, (int(width_1/2), int(height_1/2)), interpolation=cv2.INTER_CUBIC)
+
     return rotated
+
+
+if __name__ == "__main__":
+    img = cv2.imread(input("please input the picture: "))
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 对图像进行边界扩充
+    top_size, bottom_size, left_size, right_size = 50, 50, 50, 50
+    constant = cv2.copyMakeBorder(img_gray, top_size, bottom_size, left_size, right_size,
+                                  borderType=cv2.BORDER_CONSTANT, value=0)
+    rotated = rotated_img_with_fft(constant)
+
+    cv2.imshow("rotated", rotated)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
